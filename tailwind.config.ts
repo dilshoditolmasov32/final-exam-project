d9b883b5b3ece300f12a7bc295a48ffa1643aae1
Dilshodbek20040104 <dilshoditolmasov32@gmail.com>
2024-07-24 09:09:40 +0500

    exam project is  created

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,18 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      screens: {
+        xs: '480px', 
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      fontFamily:{
+        "Fira Sans":["Fira Sans"]
+      },
+  
     },
   },
   plugins: [],
